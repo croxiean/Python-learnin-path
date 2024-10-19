987dd3a76652302c6f85412854ba90475e80262b
Samet <samet.tas1634@gmail.com>
2024-10-19 17:30:25 +0300

    Add files via upload

---
+++ tuples/tuples.py
@@ -0,0 +1,17 @@
+# tuples are not changeable. Dont Forget !
+
+thistuple = ("apple","Banana",  "Cherity")
+
+print(thistuple[0]) # apple
+
+tuple = (1,"iki",True) # tuples can include diffrent variables types
+tuple2 = ("naber","gelmedi","senden",1,"Haber",True)
+print(tuple) #(1, 'iki', True)
+
+# u cant use append() method 
+
+print(tuple.count(1)) # 2
+
+print(tuple + tuple2) # (1, 'iki', True, 'naber', 'gelmedi', 'senden', 1, 'Haber', True)
+
+# tuples are more usefull to lists because tuples are net abusable.
